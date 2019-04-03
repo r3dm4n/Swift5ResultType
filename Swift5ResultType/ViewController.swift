ec2cfc90c79910b2e3641807dc915c613b879184
Alexandru Corut <r3dm4n@me.com>
2019-04-03 09:44:06 +0300

    Initial Commit

---
+++ Swift5ResultType/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5ResultType
+//
+//  Created by Alexandru Corut on 4/3/19.
+//  Copyright © 2019 Alexandru Corut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
